1fda4219ded76dbc3c0c2f3b68330aa45e43601d
luisfeliperd <luisfelipeprf@gmail.com>
2020-04-28 00:33:18 -0300

    Update wikidata_metadata.py

--- wikidata_metadata.py
+++ wikidata_metadata.py
@@ -17,32 +17,37 @@ api_url = 'https://query.wikidata.org/sparql'
 #Values to reconcile database
 tabela_df = pd.read_csv('base_mhn_teste.csv', encoding='utf-8')
 
-def reconcile_database(database):
+
+def instance_metadata(database):
     
     #Dataframe to store results
     wikiObj_df = pd.DataFrame(columns = ['metadado','instance_label','qid_instance'])
-    result_df = pd.DataFrame(columns = ['metadado','instance','qid_instance','ocurrences'])
+    result_df = pd.DataFrame(columns = ['metadado','instance','qid_instance','ocurrences', 'best_option'])
 
     #For each metadata/column on values to reconcile database
     for column in database.columns:
         start_time = datetime.now()
+        
+        print("Verificando o metadado {}".format(column))
+        
         value_count = 0
         unique_list = []
-        print("Verificando o metadado {}".format(column))
         
         #Remove duplicated values
         for value in tabela_df[column].dropna():
             
             if "||" in value:
-                for multivelue in value.split("||"):
+                
+                for multivalue in value.split("||"):
                     
-                    unique_list.append(value)
+                    unique_list.append(multivalue)
                     
             else:
+                
                 unique_list.append(value)
         
         #For each value of a column/metadata
-        for value in list(set(unique_list)):            
+        for value in list(set(unique_list)):        
                                         
                 #SPARQL query. Returns subject QID, instance QID and instance label, searching the value for subjects
                 #with matching labels.
@@ -63,9 +68,11 @@ def reconcile_database(database):
                 
                 #Verify if the JSON returns any data
                 if data == None:
+                    
                     continue
                 
                 else:
+    
                     #For each result inset data on wikiObj_df
                     for item in data['results']['bindings']:
                         
@@ -76,9 +83,7 @@ def reconcile_database(database):
                 time.sleep(3)
                 
                 value_count+=1
-        #Wait 10 seconds to avoid API block
-        #time.sleep(10)
-        
+                
         print("{} valores verificados em {} para o metadado {}".format(value_count, datetime.now()-start_time, column))
 
     #Create a new dataframe with data about metadata, wikidata instance, wikidata instance QID, and the most 5 frequent instances. 
@@ -93,7 +98,7 @@ def reconcile_database(database):
             result_df = result_df.append({'metadado':meta_df.at[meta_df_idx,'metadado'],
                                          'instance':meta_df.at[meta_df_idx,'instance_label'],
                                          'qid_instance':meta_df.at[meta_df_idx,'qid_instancia'],
-                                         'ocurrences':value}, ignore_index=True)
+                                         'ocurrences':value, 'best_option':''}, ignore_index=True)
     return result_df
 
-reconcile_database(tabela_df).to_csv("resultado.csv")
+instance_metadata(tabela_df).to_csv("instance_obj.csv", index = False)
